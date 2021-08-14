728e41ce2c0c51364db0c79ea6d522b849dacda8
fifanncher <fifanncher@gmail.com>
2021-08-14 11:30:26 +0500

    init2

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,18 +2,20 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 const Main = () => (
-  <ul>
-    <li>
-      <Link to={'/game'}>
-        {`1. Игра "Память"`}
-      </Link>
-    </li>
-    <li>
-      <Link to={'/flats'}>
-        {'2. Небольшое приложение на фреймворке'}
-      </Link>
-    </li>
-  </ul>
+  <div style={{textAlign: 'center'}}>
+    <ul>
+      <li>
+        <Link to={'/game'}>
+          {`1. Игра "Память"`}
+        </Link>
+      </li>
+      <li>
+        <Link to={'/flats'}>
+          {'2. Небольшое приложение на фреймворке'}
+        </Link>
+      </li>
+    </ul>
+  </div>
 );
 
 export default Main;
