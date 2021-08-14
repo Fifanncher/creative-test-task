306ee3b622795a9739a7587e2fddd225912f62a0
fifanncher <fifanncher@gmail.com>
2021-08-14 12:29:33 +0500

    edit cards

--- src/components/Game/cards/index.js
+++ src/components/Game/cards/index.js
@@ -1,40 +1,40 @@
 /* eslint-disable import/no-anonymous-default-export */
-import ace1 from './ace1.jpg';
-import ace2 from './ace2.jpg';
-import ace3 from './ace3.jpg';
-import ace4 from './ace4.jpg';
-import king1 from './king1.jpg';
-import king2 from './king2.jpg';
-import king3 from './king3.jpg';
-import king4 from './king4.jpg';
-import queen1 from './queen1.jpg';
-import queen2 from './queen2.jpg';
-import queen3 from './queen3.jpg';
-import queen4 from './queen4.jpg';
-import jacket1 from './jacket1.jpg';
-import jacket2 from './jacket2.jpg';
-import jacket3 from './jacket3.jpg';
-import jacket4 from './jacket4.jpg';
-import joker1 from './joker1.jpg';
-import joker2 from './joker2.jpg';
+import arnold from './arnold.jpg';
+import bart from './bart.jpg';
+import bob from './bob.jpg';
+import crubs from './crubs.jpeg';
+import flanders from './flanders.jpg';
+import gomer from './gomer.jpg';
+import goofy from './goofy.jpg';
+import kris from './kris.jpg';
+import marge from './marge.jpg';
+import meg from './meg.jpg';
+import mo from './mo.jpg';
+import osel from './osel.jpg';
+import patrick from './patrick.jpg';
+import piter from './piter.jpg';
+import quagmire from './quagmire.jpeg';
+import shrek from './shrek.jpeg';
+import stewie from './stewie.jpg';
+import stich from './stich.jpg';
 
 export default {
-  ace1,
-  ace2,
-  ace3,
-  ace4,
-  king1,
-  king2,
-  king3,
-  king4,
-  queen1,
-  queen2,
-  queen3,
-  queen4,
-  jacket1,
-  jacket2,
-  jacket3,
-  jacket4,
-  joker1,
-  joker2
+  arnold,
+  bart,
+  bob,
+  crubs,
+  flanders,
+  gomer,
+  goofy,
+  kris,
+  marge,
+  meg,
+  mo,
+  osel,
+  patrick,
+  piter,
+  quagmire,
+  shrek,
+  stewie,
+  stich
 };
